ff04732248c80421b3ae5b8cb936bcee9b3a0fca
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2026-04-26 14:48:30 +0530

    minor change in home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="p-10">
       <h1 className="text-3xl font-bold">Heedy Project</h1>
       <p className="mt-4">Frontend setup is ready 🚀</p>
+      <p className="mt-4">All are welcome 🚀</p>
     </main>
   );
 }
